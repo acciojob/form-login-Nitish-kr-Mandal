2170c7120ffbd5a5a6eeb5b2488cdc6ff211383c
Nitish Kumar Mandal <147975324+Nitish-kr-Mandal@users.noreply.github.com>
2026-01-19 15:24:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
     //Write your code here
 
+	let fname = document.getElementsByName("fname")[0].value.trim()
+	let lname = document.getElementsByName("lname")[0].value.trim()
+
+	let fullName = fname+" "+lname;
+	fullName = fullName.trim()
+
+	alert(fullName)
+
 }
